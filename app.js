a76d8ab221b246e53b809be156d5617c17e8e921
muhammadhussainharoon <hussain356q@gmail.com>
2023-10-15 13:01:11 +0500

    done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 
-const div = document.querySelector('div')
+const div = document.querySelector('#div')
 const body = document.querySelector('body')
 const array = ["red", "green", "blue", "pink" , 'yellow' , 'purple' , 'white'];
 for(i = 0; i < array.length; i++){
-div.innerHTML += `<button onclick="changecolors(${i}) class="btn">${array[i]}</button>`
+div.innerHTML += `<button onclick="changecolors(${i})">${array[i]}</button>`
 }
   function changecolors(i) {
     body.style.backgroundColor = `${array[i]}`;
